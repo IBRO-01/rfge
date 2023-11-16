897311f9df10fb4988435d833b8b01539aad9a53
IBRO-01 <151009858+IBRO-01@users.noreply.github.com>
2023-11-16 11:25:15 +0300

    Add files via upload

---
+++ www/script.js
@@ -0,0 +1,15 @@
+let car = ['Audi', 'BMW', 'Mersedes', 'Porshe', 'Nissan', 'Lada']
+let iman = ['Тупая', 'лошара', 'дебилка']
+let num = [1,2,3,4,5,6,7,8,9,7,6,5,4,3,2,4,6,0,0,0,0]
+let nums = [1,4,55,67,8,8,674,33,2,1]
+let cars = [
+    {car:'toyota', color:'green'},
+    {car:'BMW', color:'Black'},
+    {car:'Mersedes', color:'blue'},
+    {car:'Porshe', color:'red'}
+]
+
+
+
+
+ 
